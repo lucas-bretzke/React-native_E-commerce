008846edfbd717b0f9db0648150ba06c9a893c06
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-24 07:49:45 -0300

    feat: created base home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+
+
+import * as Animatable from 'react-native-animatable'
+
+export default function Login() {
+    return (
+        <View style={styles.container}>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#38a69d',
+    },
+
+})
