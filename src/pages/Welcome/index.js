70ede80f76c8c567f1ea58fb4c66778dd4b8d903
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-24 07:49:17 -0300

    refactor: separate styles in file

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
-
+import styles from './styles';
 
 import * as Animatable from 'react-native-animatable'
 import { useNavigation } from '@react-navigation/native'
@@ -26,55 +26,7 @@ export default function Welcome() {
                     onPress={() => navigation.navigate('Login')}>
                     <Text style={styles.buttonText}>Acessar</Text>
                 </TouchableOpacity>
-
             </Animatable.View>
         </View>
     );
 }
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#38a69d'
-    },
-    containerLogo: {
-        flex: 2,
-        backgroundColor: '#38a69d',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    containerForm: {
-        flex: 1,
-        backgroundColor: 'white',
-        borderTopLeftRadius: 25,
-        borderTopRightRadius: 25,
-        paddingStart: '5%',
-        paddingEnd: '5%',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 28,
-        marginBottom: 12,
-    },
-    text: {
-        color: '#a1a1a1'
-    },
-    button: {
-        position: 'absolute',
-        backgroundColor: '#38a69d',
-        borderRadius: 50,
-        paddingVertical: 8,
-        width: '60%',
-        alignSelf: 'center',
-        bottom: '15%',
-        alignItems: 'center',
-        justifycontent: 'center'
-    },
-    buttonText: {
-        fonrSize: 18,
-        color: '#fff',
-        fontWeight: 'bold'
-    }
-})
