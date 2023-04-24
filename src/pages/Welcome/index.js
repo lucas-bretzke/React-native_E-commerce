c7c66187fad18d6f41bb4569b49554958f652bcb
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-24 08:37:36 -0300

    feat: added SafeAreaView

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
 import styles from './styles';
 
 import * as Animatable from 'react-native-animatable'
 import { useNavigation } from '@react-navigation/native'
 
+import CarroC from '../../components/CarroC'
+
+
 export default function Welcome() {
     const navigation = useNavigation()
 
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <View style={styles.containerLogo}>
                 <Animatable.Image
                     animation="flipInY"
@@ -21,12 +24,14 @@ export default function Welcome() {
                 <Text style={styles.title}>Monitore, organize seus gastos de qualquer lugar!</Text>
                 <Text style={styles.text}>Faça o login para começar</Text>
 
+                <CarroC nome="Golf" />
+
                 <TouchableOpacity
                     style={styles.button}
                     onPress={() => navigation.navigate('Login')}>
                     <Text style={styles.buttonText}>Acessar</Text>
                 </TouchableOpacity>
             </Animatable.View>
-        </View>
+        </SafeAreaView>
     );
 }
