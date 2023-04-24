40ffdfa91149274e3474799ecf480fe8f60cd6b0
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-24 04:34:04 -0300

    separate styles out so component

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import styles from './styles'
 
 import * as Animatable from 'react-native-animatable'
 
+
 export default function Login() {
+
+    function visibilityButton() {
+        
+    }
+
     return (
         <View style={styles.container}>
             <Animatable.View delay={600} animation="fadeInLeft" style={styles.containerHeader}>
@@ -20,11 +26,12 @@ export default function Login() {
                 />
                 <Text style={styles.title}>Senha</Text>
                 <TextInput
+                    secureTextEntry={true}
                     placeholder="******"
                     style={styles.input}
                     placeholderTextColor={'#ccc'}
                 />
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress={visibilityButton}>
                     <Text style={styles.buttonText}>Acessar</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.buttonRegister}>
@@ -34,59 +41,3 @@ export default function Login() {
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#38a69d',
-    },
-    containerHeader: {
-        marginTop: '14%',
-        marginBottom: '8%',
-        paddingStart: '5%',
-    },
-    message: {
-        fontSize: 28,
-        fontWight: 'bold',
-        color: '#fff',
-    },
-    containerForm: {
-        backgroundColor: '#fff',
-        flex: 1,
-        borderTopLeftRadius: 25,
-        borderTopRightRadius: 25,
-        paddingStart: '5%',
-        paddingEnd: '5%',
-    },
-    title: {
-        fontSize: 20,
-        marginTop: 28,
-    },
-    input: {
-        borderBottomWidth: 1,
-        height: 40,
-        marginBottom: 12,
-        fontSize: 16
-    },
-    button: {
-        backgroundColor: '#38a69d',
-        width: '100%',
-        borderRadius: 4,
-        paddingVertical: 8,
-        marginTop: 14,
-        alignItems: 'center',
-        justifycontent: 'center'
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-    buttonRegister: {
-        marginTop: 14,
-        alignSelf: 'center'
-    },
-    registerText: {
-        color: '#a1a1a1'
-    }
-})
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,58 @@
+import { StyleSheet } from "react-native";
+
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#38a69d',
+    },
+    containerHeader: {
+        marginTop: '14%',
+        marginBottom: '8%',
+        paddingStart: '5%',
+    },
+    message: {
+        fontSize: 28,
+        fontWight: 'bold',
+        color: '#fff',
+    },
+    containerForm: {
+        backgroundColor: '#fff',
+        flex: 1,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%',
+    },
+    title: {
+        fontSize: 20,
+        marginTop: 28,
+    },
+    input: {
+        borderBottomWidth: 1,
+        height: 40,
+        marginBottom: 12,
+        fontSize: 16
+    },
+    button: {
+        backgroundColor: '#38a69d',
+        width: '100%',
+        borderRadius: 4,
+        paddingVertical: 8,
+        marginTop: 14,
+        alignItems: 'center',
+        justifycontent: 'center'
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    buttonRegister: {
+        marginTop: 14,
+        alignSelf: 'center'
+    },
+    registerText: {
+        color: '#a1a1a1'
+    }
+})
