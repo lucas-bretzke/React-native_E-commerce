172df4565511071b2f2727e21662d53fd6461ff0
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-24 17:46:32 -0300

    feat: added styles to component InputPassword

---
+++ src/components/InputPassword/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+
+export default StyleSheet.create({
+    containerInputPassword: {
+        width: '100%',
+        flexDirection: 'row',
+        borderColor: 'red'
+    },
+    input: {
+        width: '100%',
+        borderBottomWidth: 1,
+        height: 40,
+        marginBottom: 12,
+        fontSize: 16
+    },
+    eye: {
+        position: "absolute",
+        right: 0,
+        width: 30,
+        height: 30,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+})
