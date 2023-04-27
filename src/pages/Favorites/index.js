0c9b2decb3e620f5a48364c2a37ba2b6caaeff78
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-27 03:04:11 -0300

    added file favorites

---
+++ src/pages/Favorites/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+
+export default function Favorites() {
+
+    <View><Text>Favorites</Text></View>
+
+}
