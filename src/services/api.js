c06305dc1f258d7db9f03bdde14c1b527085f2bb
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-28 06:50:22 -0300

    feat: created service file

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,17 @@
-//Config axios
+import { http } from './config'
+
+export default {
+    getShoes: async () => {
+        const response = await http.get('/shoes')
+        return response?.data
+    },
+    getShoesById: async (id) => {
+        const response = await http.get(`/shoes/${id}`)
+        return response
+    },
+    putShoes: async (request) => {
+        const response = await http.put(`/shoes/${request.id}`, request)
+        return response?.data
+    }
+}
+
