03781addb6935d688b6a6d9a4eb8965f4ca45c45
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-30 03:26:33 -0300

    feat: added to server: post, put

--- src/server.js
+++ src/server.js
@@ -6,11 +6,12 @@ export function makeServer({ environment = 'development' } = {}) {
 
         models: {
             shoe: Model,
+            favorite: Model,
         },
 
         seeds(server) {
             server.create('shoe', {
-                id: 1, favorite: false, cart: false, name: "Nike Air Max Dia", price: 14090, discount: 0, img: require('./assets/1.png'), description: "O Tênis Nike Masculino Downshifter 10 traz amortecimento e suporte atualizados, para garantir uma corrida estável e confortável. Esse tênis de corrida é confeccionado em material respirável, cabedal em couro sintético"
+                id: 1, favorite: true, cart: false, name: "Nike Air Max Dia", price: 14090, discount: 0, img: require('./assets/1.png'), description: "O Tênis Nike Masculino Downshifter 10 traz amortecimento e suporte atualizados, para garantir uma corrida estável e confortável. Esse tênis de corrida é confeccionado em material respirável, cabedal em couro sintético"
             });
             server.create('shoe', {
                 id: 2, favorite: false, cart: false, name: "Nike Downshifter 10", price: 28090, discount: 10, img: require('./assets/2.png'), description: "O Tênis Nike Masculino Downshifter 10 traz amortecimento e suporte atualizados, para garantir uma corrida estável e confortável. Esse tênis de corrida é confeccionado em material respirável, cabedal em couro sintético"
@@ -27,23 +28,36 @@ export function makeServer({ environment = 'development' } = {}) {
             server.create('shoe', {
                 id: 6, favorite: false, cart: false, name: "Nike Air Max Dia", price: 92090, discount: 0, img: require('./assets/6.png'), description: "O Tênis Nike Masculino Downshifter 10 traz amortecimento e suporte atualizados, para garantir uma corrida estável e confortável. Esse tênis de corrida é confeccionado em material respirável, cabedal em couro sintético"
             });
+            server.create('favorite', {
+                id: 1, favorite: true, cart: false, name: "Nike Air Max Dia", price: 92090, discount: 0, img: require('./assets/6.png'), description: "O Tênis Nike Masculino Downshifter 10 traz amortecimento e suporte atualizados, para garantir uma corrida estável e confortável. Esse tênis de corrida é confeccionado em material respirável, cabedal em couro sintético"
+            });
         },
-        // seeds(server) {
-        //     server.create('shoe', {
-        //         id: 1, favorite: true, cart: false, name: "Nike Air Max Dia", price: 14090, discount: 0, img: require('./assets/1.png'), description: "O Tênis Nike Masculino Downshifter 10 traz amortecimento e suporte atualizados, para garantir uma corrida estável e confortável. Esse tênis de corrida é confeccionado em material respirável, cabedal em couro sintético"
-        //     });
-        // },
 
         routes() {
             this.namespace = 'api';
 
             this.get('/shoes', (schema) => {
                 return schema.shoes.all();
-            }, { timing: 2000 });
+            });
 
-            // this.get('/favorites', (schema) => {
-            //     return schema.shoes.all();
-            // }, { timing: 2000 });
+            this.put('/shoes/:id', (schema, request) => {
+                const shoeId = request.params.id;
+                const favorite = JSON.parse(request.requestBody);
+                // Atualizar o registro no banco de dados fictício
+                schema.db.shoes.update(shoeId, favorite);
+                // Retornar o registro atualizado como resposta
+                return schema.db.shoes.find(shoeId);
+              });
+              
+
+            this.get('/favorites', (schema) => {
+                return schema.favorites.all();
+            },);
+
+            this.post('/favorites', (schema, request) => {
+                let favorite = JSON.parse(request.requestBody)
+                return schema.favorites.create(favorite)
+            })
         },
     });
 
