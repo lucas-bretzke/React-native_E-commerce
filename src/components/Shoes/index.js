fdf9f4a1e155dfa7aa21bf57fa4d63e5ab4f1d6d
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-30 20:13:47 -0300

    feta:  added methods(addOrRemoveFromFavorites, editProduct), post, put, delete

--- src/components/Shoes/index.js
+++ src/components/Shoes/index.js
@@ -7,61 +7,80 @@ import axios from 'axios';
 export default function Shoes(props) {
   const [itemProperties, setProduct] = useState({ ...props });
 
-  async function addToFavorites() {
+  async function addOrRemoveFromFavorites() {
+    const setFavorite = { ...itemProperties, favorite: !itemProperties.favorite };
+
+    if (!itemProperties.favorite) {
+      try {
+        editProduct(setFavorite);
+        await axios.post('/api/favorites', setFavorite);
+      } catch (error) {
+        console.log('SHOES POST ERROR', error);
+      }
+    } else {
+      try {
+        editProduct(setFavorite);
+        await axios.delete(`api/favorites/${setFavorite.id}`, setFavorite);
+      } catch (error) {
+        console.log('SHOES POST ERROR', error);
+      } finally {
+        typeof props.getFavorites === "function" && props.getFavorites()
+      }
+    }
+  }
+
+  async function editProduct(item) {
     try {
-      const setFavorite = { ...itemProperties, favorite: true };
-      await axios.post('api/favorites/', setFavorite);
-      setProduct(setFavorite);
+      setProduct(item);
+      await axios.put(`api/shoes/${item.id}`, item);
     } catch (error) {
-      console.log(error);
+      console.log('SHOE PUT ERROR', error);
     }
   }
 
   return (
     <TouchableOpacity ableOpacity style={styles.container} onPress={props.onClick}>
       <View style={{ alignItems: 'flex-end' }}>
-        <TouchableOpacity style={styles.iconHeart} onPress={addToFavorites}>
+        <TouchableOpacity style={styles.iconHeart} onPress={addOrRemoveFromFavorites}>
           {itemProperties.favorite ?
             <Text>  <Entypo name='heart' size={21} color='#444' /></Text> :
             <Text> <Feather name='heart' size={21} color='black' /></Text>
           }
         </TouchableOpacity>
-        <Image
-          source={props.img}
-          style={styles.imgShoes}
-        />
+        <Image source={props.img} style={styles.imgShoes} resizeMode="cover" />
       </View>
-      <Text style={styles.textShoes}>
-        {/* {filterDesc(props.children)} */}
-        {props.children}
-      </Text>
+      <View style={styles.containerDescription}>
+        <Text style={styles.title}>{props.children}  {/* {filterDesc(props.children)} */}</Text>
 
-      <View style={{ flexDirection: 'row' }}>
-        {(props.discount > 0) &&
-          <Text style={styles.textShoes} >
-            R${' '} {formattedMoney(calculatesTheDiscount(props.price, props.discount))}
+        <View style={{ flexDirection: 'row' }}>
+          {(props.discount > 0) &&
+            <Text style={styles.textShoes} >
+              R${' '} {formattedMoney(calculatesTheDiscount(props.price, props.discount))}
+            </Text>
+          }
+
+          <Text style={[styles.textShoes, props.discount > 0
+            && { textDecorationLine: 'line-through', color: '#444444' }]}>
+            R${' '} {formattedMoney(props.price)}
           </Text>
-        }
+        </View>
 
-        <Text opacity={props.discount > 0 ? 0.5 : 1}
-          style={[styles.textShoes, props.discount
-            && { textDecorationLine: 'line-through' }]}>
-          R${' '} {formattedMoney(props.price)}
+        <Text style={[styles.textShoes, styles.discount]}>
+          {(props.discount > 0) && props.discount + '% off'}
         </Text>
       </View>
-
-      <Text style={[styles.textShoes, styles.discount]}>
-        {(props.discount > 0) && props.discount + '% off'}
-      </Text>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: '49.6%',
+    // paddingHorizontal: '1%',
     paddingVertical: '2.5%',
-    alignItems: 'flex-start',
+    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f6f6f6',
   },
   iconHeart: {
     paddingRight: 10,
@@ -69,13 +88,25 @@ const styles = StyleSheet.create({
     zIndex: 2,
   },
   imgShoes: {
-    width: 185,
+    width: 175,
     height: 175,
+  },
+  containerDescription: {
+    width: '100%',
+    paddingHorizontal: '5%',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginRight: 10,
+    marginVertical: '1.5%',
   },
   textShoes: {
     fontSize: 15,
     marginRight: 10,
     marginVertical: '1.5%',
+    color: 'black',
+    fontWeight: '600'
   },
   discount: {
     color: 'green'
