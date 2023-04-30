c3eea96310c69b467b2d592d5f9a3af3371d2986
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-30 20:10:52 -0300

    feta: added btn Close, navigation.addListener, unnecessary code deleted

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { View, Text, FlatList, TouchableOpacity, ScrollView } from 'react-native';
 import Shoes from '../../components/Shoes';
 import axios from 'axios';
 
 export default function Favorites() {
+    const navigation = useNavigation();
     const [favorites, setFavorites] = useState([])
 
     async function getFavorites() {
@@ -16,40 +18,41 @@ export default function Favorites() {
     }
 
     useEffect(() => {
-        getFavorites();
-    }, []);
+        const unsubscribe = navigation.addListener('focus', () => {
+            getFavorites();
+        }); return unsubscribe;
+    }, [navigation]);
 
     return (
         <ScrollView>
-            <View>
-                {favorites.length > 0 ?
-                    <View>
-                        <TouchableOpacity onPress={getFavorites}>
-                            <Text>Reload - {favorites.length}</Text>
-                        </TouchableOpacity>
+            <TouchableOpacity onPress={getFavorites}>
+                <Text>Reload - {favorites.length}</Text>
+            </TouchableOpacity>
+            {favorites.length > 0 ?
+                <View>
 
-                        < FlatList
-                            data={favorites}
-                            keyExtractor={item => item.id}
-                            renderItem={({ item }) =>
-                                <Shoes
-                                    id={item.id}
-                                    img={item.img}
-                                    cart={item.cart}
-                                    price={item.price}
-                                    favorite={item.favorite}
-                                    discount={item.discount}
-                                >
-                                    {item.name}
-                                </Shoes>
-                            }
-                        />
-                    </View>
-                    : <Text style={{ marginHorizontal: 'auto', marginTop: '50%' }}>
-                        Nenhum item adicionado aos favotitos!
-                    </Text>
-                }
-            </View >
+                    < FlatList
+                        data={favorites}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) =>
+                            <Shoes
+                                id={item.id}
+                                img={item.img}
+                                cart={item.cart}
+                                price={item.price}
+                                favorite={item.favorite}
+                                discount={item.discount}
+                                getFavorites={getFavorites}
+                            >
+                                {item.name}
+                            </Shoes>
+                        }
+                    />
+                </View>
+                : <Text style={{ marginHorizontal: 'auto', marginTop: '50%' }}>
+                    Nenhum item adicionado aos favotitos!
+                </Text>
+            }
         </ScrollView>
     )
 
