58b82b5e9ce021d4c2b90a55d7ebea684387aca0
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-30 03:24:18 -0300

    feat: list of favorites

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,10 +1,60 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, TouchableOpacity, ViewBase } from 'react-native';
 import styles from './styles';
-
+import Shoes from '../../components/Shoes';
+import * as Animatable from 'react-native-animatable'
+import axios from 'axios';
+import { ScrollView } from 'react-native-web';
 
 export default function Favorites() {
+    const [favorites, setFavorites] = useState([])
 
-    <View><Text>Favorites</Text></View>
+    useEffect(() => {
+        getFavorites();
+    }, []);
+
+    async function getFavorites() {
+        try {
+            const response = await axios.get('api/favorites/');
+            setFavorites(response.data.favorites);
+            console.log(response.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <ScrollView>
+            <Animatable.View
+                delay={600} animation="fadeInLeft">
+                {favorites.length > 0 ?
+                    <View>
+                        <TouchableOpacity onPress={getFavorites}>
+                            <Text>Reload</Text>
+                        </TouchableOpacity>
+
+                        < FlatList
+                            data={favorites}
+                            keyExtractor={item => item.id}
+                            renderItem={({ item }) =>
+                                <Shoes
+                                    // key={item.id}
+                                    img={item.img}
+                                    cart={item.cart}
+                                    price={item.price}
+                                    favorite={item.favorite}
+                                    discount={item.discount}
+                                >
+                                    {item.name}
+                                </Shoes>
+                            }
+                        />
+                    </View>
+                    :
+                    <Text style={{ marginHorizontal: 'auto', marginTop: '50%' }}>Nenhum item adicionado aos favotitos!</Text>
+                }
+            </Animatable.View >
+        </ScrollView>
+    )
 
 }
