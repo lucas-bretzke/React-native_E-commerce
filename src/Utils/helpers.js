49216dd9026d68e3ab5731d3115cdfe48e246419
Lucas Bretzke <lucasbretzke@gmail.com>
2023-05-02 15:23:27 -0300

    feat: added validateTheEmail

--- src/Utils/helpers.js
+++ src/Utils/helpers.js
@@ -7,9 +7,14 @@ const formattedMoney = (value) => (value / 100).toLocaleString('pt-BR', {
     maximumFractionDigits: 2,
 });
 
+const validateTheEmail = (email) => {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
 const calculatesTheDiscount = (value, discount) => {
     return (value - (parseFloat(discount) / 100 * value)).toFixed(0)
 }
 
 
-export { filterDesc, formattedMoney, calculatesTheDiscount }
+export { filterDesc, formattedMoney, calculatesTheDiscount, validateTheEmail }
