4721741bfc4cdbc3f2be416d086476675ce9c0d5
Lucas Bretzke <lucasbretzke@gmail.com>
2023-06-04 04:58:14 -0300

    feat: point component is now dynamic when clicking on a point

--- src/components/Dot/index.tsx
+++ src/components/Dot/index.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
 
-type dotProps = {
-  color: string,
-}
-export default function Dot(props: dotProps) {
+type DotProps = {
+  color: string;
+  isActive: boolean;
+  onPress: () => void;
+};
+
+export default function Dot(props: DotProps) {
+  const containerStyle = [styles.container, { backgroundColor: props.color },
+  props.isActive ? styles.active : null,
+  ];
+
   return (
-    <View style={[styles.container, { backgroundColor: props.color }]} />
+    <TouchableOpacity onPress={props.onPress} style={containerStyle}/>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: Dimensions.get('window').width * 0.050,
-    height: Dimensions.get('window').width * 0.050,
-    borderRadius: Dimensions.get('window').width * 0.050 / 2,
+    width: Dimensions.get('window').width * 0.05,
+    height: Dimensions.get('window').width * 0.05,
+    borderRadius: Dimensions.get('window').width * 0.05 / 2,
+    marginVertical: '6%',
     marginHorizontal: '2.5%',
     elevation: 5,
-  }
+  },
+  active: {
+    borderWidth: 2,
+    borderColor: 'black',
+  },
 });
